f5ac121f4cb0b4654f9e951cd5eb690a15e4de46
xSlayer <ahmed_the_best17@yahoo.com>
2023-04-17 15:43:45 +0200

    handling errors correctly

--- src/Main.java
+++ src/Main.java
@@ -4,74 +4,89 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        if(args.length==0)
-        {
-            throw new FileNotFoundException();
-        }
-        String Filename = args[0];
-        File input = new File(args [0]);
+    public static void main(String[] args)  {
+
+
 
         try {
-
-            error(input);
+            int L = args.length;
+            error0(L);
+            String Filename = args[0];
+            File input = new File(args [0]);
             error2(Filename);
-        } catch (EmptyAutosarFileException | NotVaildAutosarFileException ex) {
-            System.out.println("invalid");
-        }
-
-        Scanner source = new Scanner(input);
-        ArrayList<String> words = new ArrayList<String>();
-        while (source.hasNextLine()) {
-            words.add(source.nextLine());
-        }
-        source.close();
-
-        File src = new File("D:\\output.ARXML");
-        PrintWriter output = new PrintWriter(src);
-
-        ArrayList<Integer> index = new ArrayList<Integer>();
-        for (int i = 0; i < words.size(); i++) {
-            String x = words.get(i);
-            if (x.trim().startsWith("<SHORT-NAME>")) {
-                index.add(i);
+            error(input);
+            Scanner source = new Scanner(input);
+            ArrayList<String> words = new ArrayList<String>();
+            while (source.hasNextLine()) {
+                words.add(source.nextLine());
             }
-        }
+            source.close();
 
-        for (int i = 0; i < index.size(); i++) {
-            for (int j = i + 1; j < index.size(); j++) {
-                String s1 = words.get(index.get(i));
-                String s2 = words.get(index.get(j));
-                int minLength = Math.min(s1.length(), s2.length());
+            File src = new File("D:Filename_mod.ARXML");
+            PrintWriter output = new PrintWriter(src);
 
-                for (int k = 0; k < minLength; k++) {
-                    char c1 = s1.charAt(k);
-                    char c2 = s2.charAt(k);
+            ArrayList<Integer> index = new ArrayList<Integer>();
+            for (int i = 0; i < words.size(); i++) {
+                String x = words.get(i);
+                if (x.trim().startsWith("<SHORT-NAME>")) {
+                    index.add(i);
+                }
+            }
 
-                    if (c1 == c2) {
-                        continue;
-                    } else if (c1 > c2) {
-                        Collections.swap(words, index.get(i), index.get(j));
-                        break;
-                    } else {
-                        break;
+            for (int i = 0; i < index.size(); i++) {
+                for (int j = i + 1; j < index.size(); j++) {
+                    String s1 = words.get(index.get(i));
+                    String s2 = words.get(index.get(j));
+                    int minLength = Math.min(s1.length(), s2.length());
+
+                    for (int k = 0; k < minLength; k++) {
+                        char c1 = s1.charAt(k);
+                        char c2 = s2.charAt(k);
+
+                        if (c1 == c2) {
+                            continue;
+                        } else if (c1 > c2) {
+                            Collections.swap(words, index.get(i), index.get(j));
+                            break;
+                        } else {
+                            break;
+                        }
                     }
                 }
             }
+
+            for (int i = 0; i < words.size(); i++) {
+                output.println(words.get(i));
+            }
+
+            output.close();
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("There was no files passed");
+        }
+        catch (NotVaildAutosarFileException ex)
+        {
+            System.out.println("The passed file has wrong extension it should be ARXML");
+        }
+        catch (EmptyAutosarFileException ex) {
+            System.out.println("The passed file is empty");
         }
 
-        for (int i = 0; i < words.size(); i++) {
-            output.println(words.get(i));
-        }
 
-        output.close();
     }
 
-    public static void error(File x) throws EmptyAutosarFileException {
+    public static void  error0(int x) throws FileNotFoundException
+    {
+        if(x==0)
+            throw new FileNotFoundException();
+    }
+    public static void error(File x ) throws EmptyAutosarFileException {
 
         if (x.length() == 0) {
             throw new EmptyAutosarFileException();
         }
+
     }
 
     public static void error2(String x) throws NotVaildAutosarFileException {
