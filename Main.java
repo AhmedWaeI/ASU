2b3a08aebbe9e819b70800fd0f9c4ce705b0af0a
Ahmed Wael Salah <88168055+xSIayer@users.noreply.github.com>
2023-04-18 13:49:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,114 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)  {
+
+
+
+        try {
+            int L = args.length;
+            error0(L);
+            String Filename = args[0];
+            File input = new File(args [0]);
+            error2(Filename);
+            error(input);
+            Scanner source = new Scanner(input);
+            ArrayList<String> words = new ArrayList<String>();
+            while (source.hasNextLine()) {
+                words.add(source.nextLine());
+            }
+            source.close();
+            String outputname= Filename.replaceAll("\\.","_mod.");
+
+            File src = new File(outputname);
+            PrintWriter output = new PrintWriter(src);
+
+            ArrayList<Integer> index = new ArrayList<Integer>();
+            for (int i = 0; i < words.size(); i++) {
+                String x = words.get(i);
+                if (x.trim().startsWith("<SHORT-NAME>")) {
+                    index.add(i);
+                }
+            }
+
+            for (int i = 0; i < index.size(); i++) {
+                for (int j = i + 1; j < index.size(); j++) {
+                    String s1 = words.get(index.get(i));
+                    String s2 = words.get(index.get(j));
+                    int minLength = Math.min(s1.length(), s2.length());
+
+                    for (int k = 0; k < minLength; k++) {
+                        char c1 = s1.charAt(k);
+                        char c2 = s2.charAt(k);
+
+                        if (c1 == c2) {
+                            continue;
+                        } else if (c1 > c2) {
+                            Collections.swap(words, index.get(i), index.get(j));
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < words.size(); i++) {
+                output.println(words.get(i));
+            }
+
+            output.close();
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("There was no files passed");
+        }
+        catch (NotVaildAutosarFileException ex)
+        {
+            System.out.println("The passed file has wrong extension it should be ARXML");
+        }
+        catch (EmptyAutosarFileException ex) {
+            System.out.println("The passed file is empty");
+        }
+
+
+    }
+
+    public static void  error0(int x) throws FileNotFoundException
+    {
+        if(x==0)
+            throw new FileNotFoundException();
+    }
+    public static void error(File x ) throws EmptyAutosarFileException {
+
+        if (x.length() == 0) {
+            throw new EmptyAutosarFileException();
+        }
+
+    }
+
+    public static void error2(String x) throws NotVaildAutosarFileException {
+        if (!x.endsWith("ARXML")) {
+            throw new NotVaildAutosarFileException();
+        }
+    }
+}
+class EmptyAutosarFileException extends RuntimeException
+{
+    EmptyAutosarFileException(){
+        super("The file is empty");
+
+
+    }
+}
+class NotVaildAutosarFileException extends IOException {
+    NotVaildAutosarFileException()
+    {
+        super("invalid extension");
+    }
+}
+
+
